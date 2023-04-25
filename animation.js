f6f56632f9fe52c33054118ea71fad81f1c1ee18
nekoPorn <vladyslav.slovak@elev.ga.ntig.se>
2023-04-25 21:09:57 +0200

    prob final comm

--- animation.js
+++ animation.js
@@ -9,3 +9,28 @@ function toggleMenu(menu) {
       menu.style.display = "block";
     }
   }
+
+  var menuButton = document.getElementById("show-cart-btn");
+var menu = document.getElementById("cart");
+
+function togglecart(cart) {
+    var menu = document.getElementById(cart);
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+
+
+  // Get references to the button and list
+const addBtn = document.querySelector("#addBtn");
+const cartList = document.querySelector("#cart");
+
+// Add a click event listener to the button
+addBtn.addEventListener("click", function() {
+  // Create a new list item and add it to the list
+  const newItem = document.createElement("<li> hej</li>");
+  newItem.textContent = "New item";
+  cartList.appendChild(newItem);
+});
